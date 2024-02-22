d3b1d6f60b33f6d2c663f7ed517714671e0bd120
Reza Nozari <>
2024-02-22 16:12:37 +0330

    implement docker file
    - implement docker file for create database

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,19 @@
+﻿using AuctionService.Database.Context;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionService.Controllers
+{
+    [ApiController]
+    [Route("api/auctions")]
+    public class AuctionsController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly DatabaseContext _dbContext;
+        public AuctionsController(DatabaseContext context,IMapper mapper)
+        {
+            _mapper = mapper;
+            _dbContext = context;
+        }
+    }
+}
