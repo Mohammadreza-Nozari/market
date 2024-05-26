0a4cccbe87379950d124fcba4d2683b612954e75
Reza Nozari <dev.nozari@gmail.com>
2024-05-26 22:59:12 +0330

    implement RabbitMQ for Auction microservice
    - #6

--- src/AuctionService/Utilities/AuctionMapper.cs
+++ src/AuctionService/Utilities/AuctionMapper.cs
@@ -2,6 +2,7 @@
 using AuctionService.Contract.Request;
 using AuctionService.Database.Entities;
 using AutoMapper;
+using Contracts;
 
 namespace AuctionService.Utilities
 {
@@ -11,8 +12,11 @@ namespace AuctionService.Utilities
         {
             CreateMap<AuctionEntity, AuctionContract>().IncludeMembers(x => x.Item);
             CreateMap<ItemEntity, AuctionContract>();
-            CreateMap<CreateAuctionRequestContract, AuctionEntity>().ForMember(d=>d.Item,o=>o.MapFrom(s=>s));
+            CreateMap<CreateAuctionRequestContract, AuctionEntity>().ForMember(d => d.Item, o => o.MapFrom(s => s));
             CreateMap<CreateAuctionRequestContract, ItemEntity>();
+            CreateMap<AuctionContract, AuctionCreatedContract>()
+              .ForMember(dest => dest.Status, opt => opt.MapFrom(src => (int)src.Status))
+              .ForMember(dest => dest.Year, opt => opt.MapFrom(src => src.Year.ToString()));
         }
     }
 }
