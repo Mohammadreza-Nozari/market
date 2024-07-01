8c0c70f05c6b8326f616ae03c854e66460a074bf
Mohammadreza Nozari <dev.nozari@gmail.com>
2024-07-01 20:57:56 +0330

    Test

--- src/SearchService/Consumers/AuctionCreatedConsumer.cs
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -16,7 +16,7 @@ namespace SearchService.Consumers
             this._mapper = mapper;
         }
 
-        // get data 3
+        // get data 4
         public async Task Consume(ConsumeContext<AuctionCreatedContract> context)
         {
             Console.WriteLine("------> Consuming auction created: " + context.Message.Id);
